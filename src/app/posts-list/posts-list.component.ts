921fc32acb4ead24175973416a6d98d324a571a8
Hadar96 <hadarshabtay96@gmail.com>
2020-07-13 22:55:03 +0300

    commit3 squashed

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -12,8 +12,8 @@ export class PostsListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    //lets merge PRs
-    //lets mergeeeeee
+    //lets squash PRs
+    //lets mergeeeeee --NOOO
   }
 
 }
