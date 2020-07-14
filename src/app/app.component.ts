e44da4aba2c08f1d4206ea8b6bc5d88038ac6519
Hadar96 <hadarshabtay96@gmail.com>
2020-07-14 12:22:45 +0300

    commit2 temp1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'RonTheMessi-GITHUBBB';
+  title = 'RonTheMessi-GITHUBBB-temp1';
 }
